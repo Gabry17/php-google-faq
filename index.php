09f02fbd7909ba0beeb7bef16b8349a45b437998
Gabry17 <gbianchi528@gmail.com>
2022-06-09 16:16:22 +0200

    impostato header

--- index.php
+++ index.php
@@ -61,12 +61,39 @@ $main_data = [
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="style.css">
+
     <title>Google</title>
 </head>
 <body>
     <!-- header -->
-    <header> <h2>header</h2> </header>
+    <header> 
+        <div class="container flex">
+            <div class="logo flex">
+                <img src="https://www.igizmo.it/wp-content/uploads/2020/01/google_logo-696x265.png" alt="logo">
+                <h2>Privacy e termini</h2>
+            </div>
+            <div class="icon">
+                <i class="fas fa-bars"></i>
+                <span>G</span>
+            </div>
+        </div>
+        <div class="list-menu">
+            <ul class="flex">
+                <li>Introduzione</li>
+                <li>Norme sulla privacy</li>
+                <li>Termini di servizio</li>
+                <li>Tecnologie</li>
+                <li class="blue">Domande frequenti</li>
+            </ul>
+        </div>
+    </header>
     <!-- /header -->
 
     <!-- main -->
